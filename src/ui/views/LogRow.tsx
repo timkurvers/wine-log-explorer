649b61bc649bcec5c7520a5a923d339f26a0c517
Tim Kurvers <tim@moonsphere.net>
2024-07-28 00:52:41 +0200

    refactor: use new character components to visualize call stacks

--- src/ui/views/LogRow.tsx
+++ src/ui/views/LogRow.tsx
@@ -3,15 +3,65 @@ import React, { useMemo } from 'react'
 import { Badge, Group, Text } from '@mantine/core'
 
 import { type LogEntry, LogEntryType } from '../../parser/types'
-import { IconArrowLeft, IconDots, IconStackPop, IconStackPush } from '@tabler/icons-react'
+
+import Arrow from '../components/Arrow'
+import BoxCharacter from '../components/BoxCharacter'
+import SyntaxCharacter from '../components/SyntaxCharacter'
 
 interface LogRowProps {
-  style: object
+  style?: object
   entry: LogEntry
 }
 
 // TODO: Use classes
 const textSize = 14
+
+const LogRowInner = (props: LogRowProps) => {
+  const { entry } = props
+
+  if (entry.type === LogEntryType.CALL) {
+    const line = (
+      <>
+        {entry.module}
+        <SyntaxCharacter>.</SyntaxCharacter>
+        <Text fw="bold" span inherit>
+          {entry.func}
+        </Text>
+        <SyntaxCharacter>(</SyntaxCharacter>
+        {entry.args?.join(', ')}
+        <SyntaxCharacter>)</SyntaxCharacter>
+      </>
+    )
+
+    if (entry.inlinable) {
+      return (
+        <Text fz={textSize}>
+          {line} <Arrow /> {entry.return?.retval}
+        </Text>
+      )
+    }
+
+    return <Text fz={textSize}>{line}</Text>
+  } else if (entry.type === LogEntryType.RETURN) {
+    return (
+      <Group gap={0}>
+        <BoxCharacter.End />
+        <Text fz={textSize}>
+          <Arrow /> {entry.retval}
+        </Text>
+      </Group>
+    )
+  }
+
+  return (
+    <>
+      <Badge variant="transparent" color="green" p={0}>
+        {entry.channel}:{entry.logger}
+      </Badge>
+      <Text fz={textSize}>{entry.message}</Text>
+    </>
+  )
+}
 
 const LogRow = (props: LogRowProps) => {
   const { style, entry } = props
@@ -23,37 +73,29 @@ const LogRow = (props: LogRowProps) => {
       current = current?.parent
       ++level
     }
+
+    // Show return values at the same level as the initial call
+    if (entry.type === LogEntryType.RETURN) {
+      return level - 1
+    }
+
     return level
   }, [entry])
 
   return (
-    <Group style={style}>
-      <Badge variant="light">{entry.process.name || entry.process.id}</Badge>
-      <Badge variant="light" color="yellow">
+    <Group style={style} gap="sm" wrap="nowrap">
+      <Badge variant="light" flex="none">
+        {entry.process.name || entry.process.id}
+      </Badge>
+      <Badge variant="light" color="yellow" flex="none">
         {entry.thread.name || entry.thread.id}
       </Badge>
-      <Group>
+      <Group gap="sm" miw={0} wrap="nowrap">
         {Array.from({ length: indent }).map((_, index) => (
-          <IconDots size={16} opacity={0.15} key={index} />
+          <BoxCharacter.Pipe key={index} />
         ))}
-        {entry.type === LogEntryType.CALL ? (
-          <Text fz={textSize}>
-            <IconStackPop size={16} />
-            <IconStackPush size={16} />
-            {entry.module}.<strong>{entry.func}</strong>({entry.args})
-          </Text>
-        ) : entry.type === LogEntryType.RETURN ? (
-          <Text fz={textSize}>
-            <IconArrowLeft size={16} /> {entry.retval}
-          </Text>
-        ) : (
-          <>
-            <Badge variant="transparent" color="green" p={0}>
-              {entry.channel}:{entry.logger}
-            </Badge>
-            <Text fz={textSize}>{entry.message}</Text>
-          </>
-        )}
+
+        <LogRowInner entry={entry} />
       </Group>
     </Group>
   )
