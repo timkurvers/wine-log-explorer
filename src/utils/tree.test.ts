b6c9eade06411df6b41476148bfdd34a3271ace8
Tim Kurvers <tim@moonsphere.net>
2024-08-16 00:07:43 +0200

    feat: add subtree-aware compact utilities

--- src/utils/tree.test.ts
+++ src/utils/tree.test.ts
@@ -1,10 +1,10 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 
 import parseWineLog from '../parser/parseWineLog'
 
-import { calculateDepth, isVisible } from './tree'
+import { calculateDepth, compactTree, compactTreeForCall, isVisible } from './tree'
 import { stripIndent } from './strings'
-import { LogEntryCall } from '../parser/types'
+import { LogEntry, LogEntryCall } from '../parser/types'
 
 const input = stripIndent`
   00c8:00cc:class:channel:logger message 0
@@ -13,18 +13,42 @@ const input = stripIndent`
   00c8:00cc:Call innermod.func() ret=1
   00c8:00cc:class:channel:logger message 2
   00c8:00cc:class:channel:logger message 3
+  00c8:00bb:class:channel:logger message alt.1 (different thread, not part of call tree)
+  00c8:00bb:class:channel:logger message alt.2 (different thread, too)
   00c8:00cc:Ret innermod.func() retval=0 ret=1
   00c8:00cc:class:channel:logger message 4
   00c8:00cc:Ret  mod.func() retval=0 ret=0
   00c8:00cc:class:channel:logger message 5
+  00c8:00bb:class:channel:logger message alt.3 (different thread, too)
 `
+
+const filterByThread = (entry: LogEntry) => entry.thread.id === '00cc'
 
 const { entries } = await parseWineLog(input)
 
-const [msg0, , __msg1, , ____msg2, ____msg3, ____innermodFuncRet, __msg4, __modFuncRet, msg5] = entries
+const [
+  msg0,
+  ,
+  __msg1,
+  ,
+  ____msg2,
+  ____msg3,
+  altMsg1,
+  altMsg2,
+  ____innermodFuncRet,
+  __msg4,
+  __modFuncRet,
+  msg5,
+  altMsg3,
+] = entries
 
 const modFuncCall = entries[1] as LogEntryCall
 const __innermodFuncCall = entries[3] as LogEntryCall
+
+beforeEach(() => {
+  modFuncCall.isExpanded = false
+  __innermodFuncCall.isExpanded = false
+})
 
 describe('tree utilities', () => {
   describe('calculateDepth()', () => {
@@ -41,6 +65,11 @@ describe('tree utilities', () => {
       // Return entries are at the same depth as the initial call
       expect(calculateDepth(____innermodFuncRet)).toEqual(1)
       expect(calculateDepth(__modFuncRet)).toEqual(0)
+
+      // Entries in different threads are not affected by call tree
+      expect(calculateDepth(altMsg1)).toEqual(0)
+      expect(calculateDepth(altMsg2)).toEqual(0)
+      expect(calculateDepth(altMsg3)).toEqual(0)
     })
   })
 
@@ -57,6 +86,10 @@ describe('tree utilities', () => {
       expect(isVisible(__modFuncRet)).toBe(false)
       expect(isVisible(msg5)).toBe(true)
 
+      expect(calculateDepth(altMsg1)).toEqual(0)
+      expect(calculateDepth(altMsg2)).toEqual(0)
+      expect(calculateDepth(altMsg3)).toEqual(0)
+
       // This should not make a difference, as ancestors are stil hidden
       __innermodFuncCall.isExpanded = true
 
@@ -70,6 +103,10 @@ describe('tree utilities', () => {
       expect(isVisible(__msg4)).toBe(false)
       expect(isVisible(__modFuncRet)).toBe(false)
       expect(isVisible(msg5)).toBe(true)
+
+      expect(calculateDepth(altMsg1)).toEqual(0)
+      expect(calculateDepth(altMsg2)).toEqual(0)
+      expect(calculateDepth(altMsg3)).toEqual(0)
 
       // Now the entire tree is visible
       modFuncCall.isExpanded = true
@@ -85,6 +122,10 @@ describe('tree utilities', () => {
       expect(isVisible(__modFuncRet)).toBe(true)
       expect(isVisible(msg5)).toBe(true)
 
+      expect(calculateDepth(altMsg1)).toEqual(0)
+      expect(calculateDepth(altMsg2)).toEqual(0)
+      expect(calculateDepth(altMsg3)).toEqual(0)
+
       // Hide the inner call again
       __innermodFuncCall.isExpanded = false
 
@@ -98,6 +139,126 @@ describe('tree utilities', () => {
       expect(isVisible(__msg4)).toBe(true)
       expect(isVisible(__modFuncRet)).toBe(true)
       expect(isVisible(msg5)).toBe(true)
+
+      expect(calculateDepth(altMsg1)).toEqual(0)
+      expect(calculateDepth(altMsg2)).toEqual(0)
+      expect(calculateDepth(altMsg3)).toEqual(0)
+    })
+  })
+
+  describe('compactTree()', () => {
+    it('compacts given list of entries without any call subtrees', () => {
+      const visible = compactTree(entries)
+      expect(visible).toEqual([msg0, modFuncCall, altMsg1, altMsg2, msg5, altMsg3])
+    })
+
+    describe('given a filter', () => {
+      it('compacts given list of entries without any call subtrees + only entries matching given filter', () => {
+        const visibleAndFiltered = compactTree(entries, filterByThread)
+        expect(visibleAndFiltered).toEqual([msg0, modFuncCall, msg5])
+      })
+    })
+  })
+
+  describe('compactTreeForCall()', () => {
+    it('compacts given list of entries focusing on subtree for provided call', () => {
+      let visible = compactTree(entries)
+
+      // This should not make a difference, as ancestors are stil hidden
+      __innermodFuncCall.isExpanded = true
+      visible = compactTreeForCall(visible, entries, __innermodFuncCall)
+
+      expect(visible).toEqual([msg0, modFuncCall, altMsg1, altMsg2, msg5, altMsg3])
+
+      // Now the entire tree is visible
+      modFuncCall.isExpanded = true
+      visible = compactTreeForCall(visible, entries, modFuncCall)
+
+      expect(visible).toEqual([
+        msg0,
+        modFuncCall,
+        __msg1,
+        __innermodFuncCall,
+        ____msg2,
+        ____msg3,
+        altMsg1,
+        altMsg2,
+        ____innermodFuncRet,
+        __msg4,
+        __modFuncRet,
+        msg5,
+        altMsg3,
+      ])
+
+      // Hide the inner call again
+      __innermodFuncCall.isExpanded = false
+      visible = compactTreeForCall(visible, entries, __innermodFuncCall)
+
+      expect(visible).toEqual([
+        msg0,
+        modFuncCall,
+        __msg1,
+        __innermodFuncCall,
+        altMsg1,
+        altMsg2,
+        __msg4,
+        __modFuncRet,
+        msg5,
+        altMsg3,
+      ])
+    })
+
+    describe('given a filter', () => {
+      it('compacts given list of entries focusing on subtree for provided call + only entries matching given filter', () => {
+        let visibleAndFiltered = compactTree(entries, filterByThread)
+
+        // This should not make a difference, as ancestors are stil hidden
+        __innermodFuncCall.isExpanded = true
+        visibleAndFiltered = compactTreeForCall(
+          visibleAndFiltered,
+          entries,
+          __innermodFuncCall,
+          filterByThread,
+        )
+
+        expect(visibleAndFiltered).toEqual([msg0, modFuncCall, msg5])
+
+        // Now the entire tree is visible
+        modFuncCall.isExpanded = true
+        visibleAndFiltered = compactTreeForCall(visibleAndFiltered, entries, modFuncCall, filterByThread)
+
+        expect(visibleAndFiltered).toEqual([
+          msg0,
+          modFuncCall,
+          __msg1,
+          __innermodFuncCall,
+          ____msg2,
+          ____msg3,
+          ____innermodFuncRet,
+          __msg4,
+          __modFuncRet,
+          msg5,
+        ])
+
+        // Hide the inner call again
+        __innermodFuncCall.isExpanded = false
+        visibleAndFiltered = compactTreeForCall(
+          visibleAndFiltered,
+          entries,
+          __innermodFuncCall,
+          filterByThread,
+        )
+
+        expect(visibleAndFiltered).toEqual([
+          msg0,
+          modFuncCall,
+          __msg1,
+          __innermodFuncCall,
+          __msg4,
+          __modFuncRet,
+          msg5,
+        ])
+      })
     })
   })
 })
--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -1,4 +1,4 @@
-import { LogEntry, LogEntryType } from '../parser/types'
+import { LogEntry, LogEntryCall, LogEntryType } from '../parser/types'
 
 export const calculateDepth = (entry: LogEntry) => {
   let level = 0
@@ -29,4 +29,49 @@ export const isVisible = (entry: LogEntry): boolean => {
 
   // Top level (no parent) is always visible
   return true
+}
+
+interface CompactFilter {
+  (entry: LogEntry): boolean
+}
+
+// Compacts given list of all entries into a compact list without any call subtrees
+export const compactTree = (all: LogEntry[], filter?: CompactFilter) => {
+  return all.filter((entry) => isVisible(entry) && (!filter || filter(entry)))
+}
+
+// Efficiently compacts the call subtree in the given compact list
+export const compactTreeForCall = (
+  compact: LogEntry[],
+  all: LogEntry[],
+  call: LogEntryCall,
+  filter?: CompactFilter,
+) => {
+  const index = compact.indexOf(call)
+
+  // Fast bail if call entry is not in compact list
+  if (index === -1) {
+    return compact
+  }
+
+  // All entries up to and including this call entry itself (will be used as-is)
+  const pre = compact.slice(0, index + 1)
+
+  // Any expandable sub entries (or from other threads and thus visible)
+  const subtree: LogEntry[] = all
+    .slice(call.id + 1, call.return ? call.return.id + 1 : undefined)
+    .filter((entry) => isVisible(entry) && (!filter || filter(entry)))
+
+  // All entries after the return entry of this call
+  let post: LogEntry[] = []
+  if (call.return) {
+    // Need to look up return entry in all entries (may not be visible)
+    const next = all[all.indexOf(call.return) + 1]
+    const nextIndex = compact.indexOf(next)
+    if (nextIndex !== -1) {
+      post = compact.slice(nextIndex)
+    }
+  }
+
+  return pre.concat(subtree).concat(post)
 }
