9d43f77ba18af7c7c472edb8b7362f31b6f16c51
Tim Kurvers <tim@moonsphere.net>
2024-08-16 00:14:35 +0200

    fix: only display process name when available in thread filter

--- src/ui/views/Log.tsx
+++ src/ui/views/Log.tsx
@@ -218,7 +218,7 @@ const Log = (props: LogProps) => {
         <MultiSelect
           clearable
           data={filteredProcesses.map((process) => ({
-            group: `${process.name} (${process.id})`,
+            group: process.name ? `${process.name} (${process.id})` : process.id,
             items: process.threads.map((thread) => ({
               value: thread.id,
               label: thread.name || thread.id,
